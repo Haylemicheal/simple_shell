6d7816eb2f46c841b41d2c784586407062784ec2
MubarekHussen <mubahussen2014@gmail.com>
2022-05-14 15:04:16 +0300

    creating_a_header

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+
+#endif /*_SHELL_H_*/
