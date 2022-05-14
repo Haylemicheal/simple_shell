1000647fdabe5db3f0f45d3de2494c596f4589e4
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-05-14 20:44:52 +0300

    Add shell c file

---
+++ shell.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * main - main function of the shell
+ * @argc: Size of arguments
+ * @argv: Arguments array
+ * Return: Success (0)
+ */
+int main(int argc, char **argv)
+{
+	char *buffer = NULL;
+	size_t buffsize = 0;
+	size_t line = 0;
+
+	do {
+		prompt();
+		line = getline(&buffer, &buffsize, stdin);
+		if (line == EOF)
+		{
+			free(buffer);
+			_putchar('\n');
+			return (EXIT_SUCCESS);
+		}
+	} while (line != EOF);
+
+	return (EXIT_SUCCESS);
+}
+
